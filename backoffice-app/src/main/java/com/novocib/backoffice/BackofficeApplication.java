c9a2ae421bf693728c48860d72b03199b5239703
alexbalak21 <alex.balak@outlook.com>
2026-05-24 16:19:35 +0200

    fixed the modules

--- backoffice-app/src/main/java/com/novocib/backoffice/BackofficeApplication.java
+++ backoffice-app/src/main/java/com/novocib/backoffice/BackofficeApplication.java
@@ -3,6 +3,7 @@ package com.novocib.backoffice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+
 import com.novocib.stocks.StocksModuleConfig;
 import com.novocib.timetracking.TimeTrackingModuleConfig;
 
@@ -12,7 +13,8 @@ import com.novocib.timetracking.TimeTrackingModuleConfig;
     TimeTrackingModuleConfig.class
 })
 public class BackofficeApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BackofficeApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackofficeApplication.class, args);
+    }
 }
